1d60660093973aaf4fb6cd2ad6e9bcb469a00cda
Guilherme Louro <guilherme-louro@hotmail.com>
2015-09-10 15:58:46 -0300

    items

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.text import capfirst
 
 class Product(models.Model):
     name = models.CharField(_('Nome'), max_length=500)
@@ -12,7 +13,7 @@ class Product(models.Model):
     def to_dict_json(self):
         return {
             'id': self.id,
-            'name': self.name,
+            'name': capfirst(self.name.lower()),
             'priority': self.priority
         }
 
