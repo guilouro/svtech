025b5bfa5f4f791b618a84793ba419f66735eb78
Guilherme Louro <guilherme-louro@hotmail.com>
2015-09-11 15:01:45 -0300

    setpriority

--- _src/assets/js/controllers/priority.js
+++ _src/assets/js/controllers/priority.js
@@ -1,7 +1,13 @@
 angular.module('svApp')
     .controller('PriorityController', ['$scope', '$http', function($scope, $http) {
+
         $scope.test = '';
         $http.get('/list/').success(function(data){
             $scope.products = data.products;
         });
+
+        $scope.setPriority = function() {
+            console.log($scope.products);
+        }
+
     }]);
